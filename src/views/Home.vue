ac5159268a3269f742c2bc0bd0ec4e51d0506252
MofuMofu <froakie002@gmail.com>
2021-11-16 19:00:40 +0900

    feature: #3 入力項目を追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <b-form>
+      <b-form-group id="kind-group" label="種類"
+        ><b-form-select id="kind-select"></b-form-select
+      ></b-form-group>
+      <b-form-group id="title-group" label="タイトル"
+        ><b-form-input id="title-form"></b-form-input
+      ></b-form-group>
+      <b-form-group id="link-group" label="URLリンク"
+        ><b-form-input id="link-form"></b-form-input
+      ></b-form-group>
+      <b-form-group id="emort-group" label="気持ち"
+        ><b-form-select id="emort-form"></b-form-select
+      ></b-form-group>
+      <b-form-group id="description-group" label="どんなところが"
+        ><b-form-textarea id="description-form"></b-form-textarea
+      ></b-form-group>
+      <b-form-group id="emort-group" label="何を感じたか"
+        ><b-form-textarea id="emort-form"></b-form-textarea
+      ></b-form-group>
+      <b-form-group id="complement-group" label="補足"
+        ><b-form-textarea id="complement-form"></b-form-textarea
+      ></b-form-group>
+    </b-form>
+  </div>
 </template>
 
 <script lang="ts">
