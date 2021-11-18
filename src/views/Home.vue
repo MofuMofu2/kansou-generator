3720fc780dd7e7ca8c1fec1e7325985dff0fed10
MofuMofu <froakie002@gmail.com>
2021-11-18 18:31:23 +0900

    feature: #5 フォーム入力値のdataを生成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,25 +2,25 @@
   <div class="home">
     <b-form class="m-5">
       <b-form-group id="kind-group" label="種類">
-        <b-form-select id="kind-select" />
+        <b-form-select id="kind-select" v-model="form.kind" />
       </b-form-group>
       <b-form-group id="title-group" label="タイトル"
-        ><b-form-input id="title-form" />
+        ><b-form-input id="title-form" v-model="form.title" />
       </b-form-group>
       <b-form-group id="link-group" label="URLリンク"
-        ><b-form-input id="link-form" />
+        ><b-form-input id="link-form" v-model="form.link" />
       </b-form-group>
       <b-form-group id="emort-group" label="気持ち"
-        ><b-form-select id="emort-form" />
+        ><b-form-select id="emort-form" v-model="form.emort" />
       </b-form-group>
       <b-form-group id="description-group" label="どんなところが"
-        ><b-form-textarea id="description-form" />
+        ><b-form-textarea id="description-form" v-model="form.description" />
       </b-form-group>
-      <b-form-group id="emort-group" label="何を感じたか"
-        ><b-form-textarea id="emort-form" />
+      <b-form-group id="emotion-group" label="何を感じたか"
+        ><b-form-textarea id="emotion-form" v-model="form.emotion" />
       </b-form-group>
       <b-form-group id="complement-group" label="補足"
-        ><b-form-textarea id="complement-form" />
+        ><b-form-textarea id="complement-form" v-model="form.complement" />
       </b-form-group>
       <b-form-row>
         <b-col cols="10" />
@@ -40,5 +40,15 @@ import { Component, Vue } from "vue-property-decorator";
 @Component({
   components: {},
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  form = {
+    kind: null,
+    title: "",
+    link: "",
+    emort: "",
+    description: "",
+    emotion: "",
+    complement: "",
+  };
+}
 </script>
