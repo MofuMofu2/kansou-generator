8b56b544dfc25e86635d77c1853a48b1540f1d5f
MofuMofu <froakie002@gmail.com>
2021-12-13 22:14:34 +0900

    feature: #5 テスト追加

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,13 +1,15 @@
 import { shallowMount } from "@vue/test-utils";
 import KansouForms from "@/components/KansouForms.vue";
-import BootstrapVue from "bootstrap-vue";
+import BootstrapVue, { BFormInput } from "bootstrap-vue";
 import Vue from "vue";
 
 Vue.use(BootstrapVue);
 
 describe("KansouForms.vue", () => {
-  it("テストが動くこと", () => {
+  it("必須入力項目に関するテスト", () => {
     const wrapper = shallowMount(KansouForms);
-    expect(wrapper.exists()).toBeTruthy();
+    expect(wrapper.findAllComponents(BFormInput).at(0).classes()).toContain(
+      "is-invalid"
+    );
   });
 });
