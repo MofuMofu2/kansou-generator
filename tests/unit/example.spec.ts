b085730d8693318a6d6222c4f69dc6ce6f24eb01
MofuMofu <froakie002@gmail.com>
2021-12-13 21:37:42 +0900

    fix: #5 BootStrapVueをVue.useでモックしないと警告になってしまう

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,5 +1,9 @@
 import { shallowMount } from "@vue/test-utils";
 import KansouForms from "@/components/KansouForms.vue";
+import BootstrapVue from "bootstrap-vue";
+import Vue from "vue";
+
+Vue.use(BootstrapVue);
 
 describe("KansouForms.vue", () => {
   it("テストが動くこと", () => {
